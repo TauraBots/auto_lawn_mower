cb7049931fcedb5b5eec6a8080b619d98fe56148
Bruno Castro <38269840+brunoocastro@users.noreply.github.com>
2019-03-27 22:05:46 -0300

    Movimentação Basica do Turtle COMENTADA

--- move_base.py
+++ move_base.py
@@ -1,21 +1,22 @@
-import rospy
-from geometry_msgs.msg import Twist
-import sys, select, termios, tty
+import rospy 					#Biblioteca do ROS pra Python
+from geometry_msgs.msg import Twist		#Node(eu acho) das mensagens que é responsavel pelas movimentações do turtle, o Twist, dentro dele que estão as variaveis de velocidades!
 
-twist = Twist()
-rospy.init_node("teste")
-pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+vel = Twist()					#Define "vel" como atalho para a função Twist() 
+rospy.init_node("teste")			#Inicia um Node para esse codigo (Coisas do ROS que ainda não sei bem, mas pra esse codigo não é necessario por enquanto!
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10) #Define o publisher, ele que envia todas modificações feitas nas variaveis para o robo! 
+#Se não publicar, o valor alterado não é aplicado no robo!
+#Pelo que entendi, o Queue = 10 significa que ele armazena até 10 modificações pra enviar, algo do tipo
 
 
-while not rospy.is_shutdown():
-
-	twist.linear.x = 1.8
-	twist.linear.y = 0.0
-	twist.linear.z = 0.0
+while not rospy.is_shutdown():  #Enquanto o rospy não for encerrado roda o codigo (Tipo o main desse codigo)
+	#Ele ta só andando pra frente com a velocidade linear X de 1
+	vel.linear.x = 1.0	#Velocidade Linear X
+	vel.linear.y = 0.0	#Velocidade Linear Y
+	vel.linear.z = 0.0	#Velocidade Linear Z
+        #tem que descobrir o limite dessas velocida
+	vel.angular.x = 0.0	#Velocidade Angular X
+	vel.angular.y = 0.0	#Velocidade Angular Y
+	vel.angular.z = 0.0	#Velocidade Angular Z
         
-	twist.angular.x = 1.0
-	twist.angular.y = 0.0
-	twist.angular.z = 1.0
-        
-        pub.publish(twist)
+        pub.publish(vel)	#Publica todas as modificações
 
